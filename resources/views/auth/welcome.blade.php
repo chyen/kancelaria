a2f8e36e4b678cc4fdb98148f4c7107f364ed4e3
mateusz rzosinski <mrzosinski@9bits.pl>
2016-04-30 17:06:11 +0200

    podstawka

--- resources/views/auth/welcome.blade.php
+++ resources/views/auth/welcome.blade.php
@@ -2,14 +2,44 @@
 
 @section('content')
 
+{!!Html::style('css/global.css')!!}
+<!--<link rel="stylesheet" href="{{ asset('asset/css/global.css') }}" type="text/css">-->
+
 <div class='container'>
     <div class='col-sm-12'>
-        <div class='col-sm-6'>
-        </div>
-        <div class='col-sm-6'>
-            LoremIpsum
+        <div class="panel panel-default panel-main">
+            <div class="exp"></div>
+            <div class="main-panel-title">
+                <div>Kancelaria Adwokacka Pawelczyk</div>
+                <div class="extended-title">In criminalibus probationes debent esse luce clariones</div>
+            </div>
         </div>
 
+    </div>
+    <div class="col-sm-12">
+        <div class="col-sm-9">
+            <div class="panel panel-default panel-content">
+                <p>Kancelaria Adwokacka Pawelczyk Adwokata Janusza Pawelczyk świadczy pomoc prawną,
+                    obejmującej swym zakresem większość dziedzin obwiązującego prawa dla podmiotów gospodarczych
+                    oraz osób prywatnych ceniących sobie bezpieczeństwo, komfort, jak również dyskrecję w realizacji powierzonych zadań.
+                </p>
+                <p>Specjalizujemy się w prawie karnym, ze szczególnym uwzględnieniem szerokorozumianej dziedziny prawa gospodarczego.</p>
+                <p>Udzielamy pomocy prawnej na każdym etapie postępowania, zarówno przed Sądami i innymi organami państwowymi oraz samorządowymi.</p>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="panel panel-default panel-contact">
+                <div class="contact-text">
+                    <p><b>Dane kontakowe: </b></p>
+                    <p>Kancelaria Adwokacka Pawelczyk</p>
+                    <p>Adwokat Janusz Pawelczyk</p>
+                    <p class="address">tel. +48 500-448-001</p>
+                    <p class="address">e-mail biuro@kancelariapawelczyk.pl</p>
+                    <p class="address">ul. Jana Kilińskiego 5</p>
+                    <p class="address">07-410 Ostrołęka</p>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
