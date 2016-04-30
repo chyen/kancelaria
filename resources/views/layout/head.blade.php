fe8f3f01a472ccd251a765641b06d754cbbeb994
mateusz rzosinski <mrzosinski@9bits.pl>
2016-04-30 17:34:13 +0200

    border i html title

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -1,6 +1,6 @@
 @section('head')
 <head>
-    <title>Lorem ipsum</title>
+    <title>Kancelaria Adwokacka Pawelczyk</title>
     @include('layout.master-css')
     @include('layout.master-fonts')
     @include('layout.master-js')
@@ -10,11 +10,8 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta content="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." name="description" />
-    <meta content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae enim gravida, pellentesque turpis tincidunt, sodales velit.
-          Sed id quam vel dui consequat aliquam. Nulla egestas eu mauris non bibendum. Nunc feugiat, ex ac ullamcorper placerat, massa odio condimentum justo, sit amet gravida lorem elit nec diam.
-          Nulla quis metus vel sem consequat porttitor vel eget mi. Quisque ultricies mauris lacus, vel rhoncus lorem tristique eget. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-          In ullamcorper, arcu efficitur lacinia volutpat, purus nisi porta diam, a tempus justo nulla vel purus. Sed odio elit, pretium eget consequat ut, pretium at dolor.
-          Maecenas maximus quam ligula, euismod pellentesque ante ultrices eu. Quisque ultricies tortor id magna suscipit, nec bibendum ligula sagittis. Aenean pellentesque orci velit, at rhoncus sapien laoreet vel.
-          Vivamus viverra ante vel mauris sollicitudin, ac accumsan ligula condimentum. Integer aliquam ac dolor eget auctor." name="keywords"/>
+    <meta content="Kancelaria Adwokacka Pawelczyk Adwokata Janusza Pawelczyk świadczy pomoc prawną,
+          obejmującą swym zakresem większość dziedzin obwiązującego prawa dla podmiotów gospodarczych
+          oraz osób prywatnych ceniących sobie bezpieczeństwo, komfort, jak również dyskrecję w realizacji powierzonych zadań." name="keywords"/>
 </head>
 @show
